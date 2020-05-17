57c4abf48dcd914527d664d56e4119bf768237cf
Jared Huffstutler <jaredhuff85@gmail.com>
2020-05-16 23:11:08 -0700

    Updates

---
+++ precache-manifest.6ef1500983e8262c28a5510df0dc5823.js
@@ -0,0 +1,286 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9c78fa057a47b8e1b997c4883dde303",
+    "url": "/texas-holdem/index.html"
+  },
+  {
+    "revision": "fdfdd14dcae5aacf8a3c",
+    "url": "/texas-holdem/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "8e6a726c9ca40bc45e56",
+    "url": "/texas-holdem/static/css/main.054a1dba.chunk.css"
+  },
+  {
+    "revision": "fdfdd14dcae5aacf8a3c",
+    "url": "/texas-holdem/static/js/2.ac8a699f.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/texas-holdem/static/js/2.ac8a699f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e6a726c9ca40bc45e56",
+    "url": "/texas-holdem/static/js/main.680b81f8.chunk.js"
+  },
+  {
+    "revision": "5167e06238b70acac3fd",
+    "url": "/texas-holdem/static/js/runtime-main.9ad20811.js"
+  },
+  {
+    "revision": "354b7b1e2da2c73ea0778c47d5a890de",
+    "url": "/texas-holdem/static/media/10C.354b7b1e.jpg"
+  },
+  {
+    "revision": "13a231b8733a390aa28450b42ec16c15",
+    "url": "/texas-holdem/static/media/10D.13a231b8.jpg"
+  },
+  {
+    "revision": "d0a839fea541b8b56a8ef814d3689d8c",
+    "url": "/texas-holdem/static/media/10H.d0a839fe.jpg"
+  },
+  {
+    "revision": "c1302c56e3108da27c153af31905d84e",
+    "url": "/texas-holdem/static/media/10S.c1302c56.jpg"
+  },
+  {
+    "revision": "066ae686ea05806c36d3acc7136fb6b2",
+    "url": "/texas-holdem/static/media/2C.066ae686.jpg"
+  },
+  {
+    "revision": "c6677ab40b82c906193806c2397c1ad3",
+    "url": "/texas-holdem/static/media/2D.c6677ab4.jpg"
+  },
+  {
+    "revision": "4b2cf8096b2dc35e2ac94ef081d01c76",
+    "url": "/texas-holdem/static/media/2H.4b2cf809.jpg"
+  },
+  {
+    "revision": "8e24cbaa081f61c390c4fbf153c1c6b6",
+    "url": "/texas-holdem/static/media/2S.8e24cbaa.jpg"
+  },
+  {
+    "revision": "38e0df6610e78f56930230621908562f",
+    "url": "/texas-holdem/static/media/3C.38e0df66.jpg"
+  },
+  {
+    "revision": "13159968babd5f960e451a2f71bb08c9",
+    "url": "/texas-holdem/static/media/3D.13159968.jpg"
+  },
+  {
+    "revision": "b1db92f57ebb35db394406af9f4b13d0",
+    "url": "/texas-holdem/static/media/3H.b1db92f5.jpg"
+  },
+  {
+    "revision": "ec5993d00131ddc3cb607ad1f8818ff1",
+    "url": "/texas-holdem/static/media/3S.ec5993d0.jpg"
+  },
+  {
+    "revision": "78b8df72f82dcd7f48544f4e8c7fa518",
+    "url": "/texas-holdem/static/media/4C.78b8df72.jpg"
+  },
+  {
+    "revision": "36b51a18e2d3acb7a532ccb5c563c4b5",
+    "url": "/texas-holdem/static/media/4D.36b51a18.jpg"
+  },
+  {
+    "revision": "8fccba55ad0185cae67fbf191a92121a",
+    "url": "/texas-holdem/static/media/4H.8fccba55.jpg"
+  },
+  {
+    "revision": "a4b73fffd1d3dde1b3a071c0ef7ba603",
+    "url": "/texas-holdem/static/media/4S.a4b73fff.jpg"
+  },
+  {
+    "revision": "06b943fb08c8fb321a5fbeb2063fd631",
+    "url": "/texas-holdem/static/media/5C.06b943fb.jpg"
+  },
+  {
+    "revision": "10d896190db993a854fa54cb18d1c144",
+    "url": "/texas-holdem/static/media/5D.10d89619.jpg"
+  },
+  {
+    "revision": "f5d702f9cd1c7067d01d34fa56c90ad8",
+    "url": "/texas-holdem/static/media/5H.f5d702f9.jpg"
+  },
+  {
+    "revision": "8fd39dd72d5c5371f831c5a77e8bcbb8",
+    "url": "/texas-holdem/static/media/5S.8fd39dd7.jpg"
+  },
+  {
+    "revision": "9d135342118365eeae81d6cb70705c29",
+    "url": "/texas-holdem/static/media/6C.9d135342.jpg"
+  },
+  {
+    "revision": "9ed6948df11434d0899379119fbf8d52",
+    "url": "/texas-holdem/static/media/6D.9ed6948d.jpg"
+  },
+  {
+    "revision": "c78428894258ba7c45d435d5738038ce",
+    "url": "/texas-holdem/static/media/6H.c7842889.jpg"
+  },
+  {
+    "revision": "f14994716560b2c73e9048f6d8911ed3",
+    "url": "/texas-holdem/static/media/6S.f1499471.jpg"
+  },
+  {
+    "revision": "57202bbbfdbd34eedc0a119f01c39c5a",
+    "url": "/texas-holdem/static/media/7C.57202bbb.jpg"
+  },
+  {
+    "revision": "3d5f65826654ca4611cddd54868ad9db",
+    "url": "/texas-holdem/static/media/7D.3d5f6582.jpg"
+  },
+  {
+    "revision": "25680783dfa2cd51989ed1c519a8904f",
+    "url": "/texas-holdem/static/media/7H.25680783.jpg"
+  },
+  {
+    "revision": "8e9fad3aaac1607ffff6d692e9c35ad2",
+    "url": "/texas-holdem/static/media/7S.8e9fad3a.jpg"
+  },
+  {
+    "revision": "b139c1671a0c0460fc83101bc3e7a44c",
+    "url": "/texas-holdem/static/media/8C.b139c167.jpg"
+  },
+  {
+    "revision": "d5830921b1e34fa05481fdf0ad00f7bf",
+    "url": "/texas-holdem/static/media/8D.d5830921.jpg"
+  },
+  {
+    "revision": "424248abcc02f129ce31e2b595d98a3a",
+    "url": "/texas-holdem/static/media/8H.424248ab.jpg"
+  },
+  {
+    "revision": "b824de6ccddcbf3a7250733b0ee06237",
+    "url": "/texas-holdem/static/media/8S.b824de6c.jpg"
+  },
+  {
+    "revision": "79c5345d04f83165dd2e2ed76c9094cf",
+    "url": "/texas-holdem/static/media/9C.79c5345d.jpg"
+  },
+  {
+    "revision": "4de857aa75cc79d132d3047e52ac7aa7",
+    "url": "/texas-holdem/static/media/9D.4de857aa.jpg"
+  },
+  {
+    "revision": "6df56ea434f20d98ed3531f08487c2ee",
+    "url": "/texas-holdem/static/media/9H.6df56ea4.jpg"
+  },
+  {
+    "revision": "70f9f3964221bca5704de1e03c2a6d76",
+    "url": "/texas-holdem/static/media/9S.70f9f396.jpg"
+  },
+  {
+    "revision": "fd0f054bb979d2ac9a90ef04af2e73b2",
+    "url": "/texas-holdem/static/media/AC.fd0f054b.jpg"
+  },
+  {
+    "revision": "7913ce4a734a1ee6f676b562ec6385a6",
+    "url": "/texas-holdem/static/media/AD.7913ce4a.jpg"
+  },
+  {
+    "revision": "c5b6a134490321cf2ecb1cc312641a07",
+    "url": "/texas-holdem/static/media/AH.c5b6a134.jpg"
+  },
+  {
+    "revision": "272af4045d71c85d7455e1bcc0d5193b",
+    "url": "/texas-holdem/static/media/AS.272af404.jpg"
+  },
+  {
+    "revision": "243e0fb7790239b34497b0445060e9be",
+    "url": "/texas-holdem/static/media/Gray_back.243e0fb7.jpg"
+  },
+  {
+    "revision": "4dfabab2e55256d817bfaf2b61713b55",
+    "url": "/texas-holdem/static/media/Green_back.4dfabab2.jpg"
+  },
+  {
+    "revision": "6d00e155ffb19a90b591fb8667e5c82f",
+    "url": "/texas-holdem/static/media/JC.6d00e155.jpg"
+  },
+  {
+    "revision": "fea5118a84bd568822820839c5c86fc1",
+    "url": "/texas-holdem/static/media/JD.fea5118a.jpg"
+  },
+  {
+    "revision": "454bbb768f2decf629a5b6386943377c",
+    "url": "/texas-holdem/static/media/JH.454bbb76.jpg"
+  },
+  {
+    "revision": "686aea084843ab61f228a0caf63f76e2",
+    "url": "/texas-holdem/static/media/JS.686aea08.jpg"
+  },
+  {
+    "revision": "825911bf7df5644fa3bfc3f7229beb87",
+    "url": "/texas-holdem/static/media/KC.825911bf.jpg"
+  },
+  {
+    "revision": "12084ce552decbd2f2e85d2954a8c95f",
+    "url": "/texas-holdem/static/media/KD.12084ce5.jpg"
+  },
+  {
+    "revision": "39e16dba7d707da4dc0c504b9c07acf5",
+    "url": "/texas-holdem/static/media/KH.39e16dba.jpg"
+  },
+  {
+    "revision": "d710f10ca92c83d86404fa0b3f0d2014",
+    "url": "/texas-holdem/static/media/KS.d710f10c.jpg"
+  },
+  {
+    "revision": "e40a157bb37e88c43b827cde1931c3de",
+    "url": "/texas-holdem/static/media/QC.e40a157b.jpg"
+  },
+  {
+    "revision": "81deb60685647144824b7db38441e38e",
+    "url": "/texas-holdem/static/media/QD.81deb606.jpg"
+  },
+  {
+    "revision": "87f96511ae89bdc9da017dbea3881e73",
+    "url": "/texas-holdem/static/media/QH.87f96511.jpg"
+  },
+  {
+    "revision": "5b6749fd8039f505f88a11782e3007f2",
+    "url": "/texas-holdem/static/media/QS.5b6749fd.jpg"
+  },
+  {
+    "revision": "cc10d8e1478c0a3de97fccef1df1ffa5",
+    "url": "/texas-holdem/static/media/Red_back.cc10d8e1.jpg"
+  },
+  {
+    "revision": "8788b8faf7cbaee19aa82f83433d2bb0",
+    "url": "/texas-holdem/static/media/Yellow_back.8788b8fa.jpg"
+  },
+  {
+    "revision": "aff124e4c989a9756a0484a860ebf770",
+    "url": "/texas-holdem/static/media/aces.aff124e4.jpg"
+  },
+  {
+    "revision": "0d1d815b25481659910be1064e65ac12",
+    "url": "/texas-holdem/static/media/back_cards-07.0d1d815b.jpg"
+  },
+  {
+    "revision": "d9e0d11ee6846f418f5a71617d2fc0f9",
+    "url": "/texas-holdem/static/media/blue_back.d9e0d11e.jpg"
+  },
+  {
+    "revision": "b07aa1855426db5763b4c3a4d4c134e5",
+    "url": "/texas-holdem/static/media/honor_clubs.b07aa185.jpg"
+  },
+  {
+    "revision": "f6026990b55ce98afe45ef19ff63d1f9",
+    "url": "/texas-holdem/static/media/honor_diamond.f6026990.jpg"
+  },
+  {
+    "revision": "3458f8cd56b5fc02c5d1f53cee20c296",
+    "url": "/texas-holdem/static/media/honor_heart-14.3458f8cd.jpg"
+  },
+  {
+    "revision": "680159881e28e64fdc2a791909bf870d",
+    "url": "/texas-holdem/static/media/honor_spade.68015988.jpg"
+  },
+  {
+    "revision": "438c2e78ebccc71091f17d4351ad58f2",
+    "url": "/texas-holdem/static/media/purple_back.438c2e78.jpg"
+  }
+]);
